52c7180f04f73d27d0b4666e12381d002d50b50e
hagmal <mahag2193@oslomet.no>
2025-09-11 10:15:56 +0200

    Lagt til knappene på startskjermen

--- app/src/main/java/com/example/numbino_s305896/ui/sider/StartSkjerm.kt
+++ app/src/main/java/com/example/numbino_s305896/ui/sider/StartSkjerm.kt
@@ -1,12 +1,15 @@
 package com.example.numbino_s305896.ui.sider
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,6 +33,20 @@ fun StartSkjermen (
         StartKnappen(
             tekst = stringResource(id = R.string.start_spill),
             onClick = klikkStartSpill
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        StartKnappen(
+            tekst = stringResource(id = R.string.om_spill),
+            onClick = klikkOmSpill
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        StartKnappen(
+            tekst = stringResource(id = R.string.preferanser),
+            onClick = klikkPreferanser
         )
     }
 }
