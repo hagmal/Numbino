1e1cd02ada12ef971c39cc5817b1ac484b135f68
hagmal <mahag2193@oslomet.no>
2025-09-11 16:53:27 +0200

    Endret på spillskjerm.

--- app/src/main/java/com/example/numbino_s305896/ui/sider/SpillSkjerm.kt
+++ app/src/main/java/com/example/numbino_s305896/ui/sider/SpillSkjerm.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -108,12 +109,6 @@ fun SpillSkjermen (
         ) {
             NummerKnappen(nummer = 0, vedKlikk = vedTallKlikk)
         }
-
-        Spacer(modifier = Modifier.height(32.dp))
-
-        // Avbryt knapp
-
-
     }
 
 }
