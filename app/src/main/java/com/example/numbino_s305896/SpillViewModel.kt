1a736f1babfed38ae977a500ac6c2dab0a3fa3c1
hagmal <mahag2193@oslomet.no>
2025-09-20 20:41:23 +0200

    Startet på ViewModelen

--- app/src/main/java/com/example/numbino_s305896/SpillViewModel.kt
+++ app/src/main/java/com/example/numbino_s305896/SpillViewModel.kt
@@ -1,14 +1,44 @@
 package com.example.numbino_s305896
 
 import androidx.lifecycle.ViewModel
+import com.example.numbino_s305896.ui.SpillUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-data class SpillUiTilstand (
-    val nåværendeSpørsmål: String = "",
-    val nåværendeSvar: Int = 0,
-    val tilbakemelding: Int = 3,
-    val trykketNummer: Int? = null,
-    val nåværendeSpørsmålIndeks: Int = 0
-)
 
 class SpillViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SpillUiState())
+    val uiState: StateFlow<SpillUiState> = _uiState.asStateFlow()
+
+    private var alleRegnestykker: List<String> = emptyList()
+    private lateinit var currentRegnestykke: String
+    private var brukteRegnestykker: MutableSet<String> = mutableSetOf()
+
+
+    private fun velgTilfeldigRegnestykke(): String {
+        currentRegnestykke = alleRegnestykker.random()
+        if (brukteRegnestykker.contains(currentRegnestykke)) {
+            return velgTilfeldigRegnestykke()
+        } else {
+            brukteRegnestykker.add(currentRegnestykke)
+            return currentRegnestykke
+        }
+    }
+
+    fun nullstillSpill() {
+        brukteRegnestykker.clear()
+        if (alleRegnestykker.isEmpty()) {
+            _uiState.value = SpillUiState()
+            return
+        }
+        val valgt = velgTilfeldigRegnestykke()
+        _uiState.value = SpillUiState(
+            regnestykke = valgt,
+            brukerSvar = "",
+            tilbakemelding = 3,
+            ferdig = false
+        )
+    }
 }
---
+++ app/src/main/java/com/example/numbino_s305896/ui/SpillUiState.kt
@@ -0,0 +1,8 @@
+package com.example.numbino_s305896.ui
+
+data class SpillUiState (
+    val regnestykke: String = "",
+    val brukerSvar: String = "",
+    val tilbakemelding: Int = 3,
+    val ferdig: Boolean = false
+)
