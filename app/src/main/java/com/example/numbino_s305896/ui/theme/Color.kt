ae839d73b4571ecf9bdcdd0efc5191f19e04b721
hagmal <mahag2193@oslomet.no>
2025-09-24 13:16:39 +0200

    Lagt inn fargetemaer

--- app/src/main/java/com/example/numbino_s305896/ui/theme/Color.kt
+++ app/src/main/java/com/example/numbino_s305896/ui/theme/Color.kt
@@ -2,10 +2,28 @@ package com.example.numbino_s305896.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Hovedfarge (viktige knapper, figuren)
+val OransjeHovedfarge = Color(0xFFFFB74D)
+val OnOransje = Color (0xFF000000) // Svart tekst på oransje
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Sekundær (topbar, mindre viktige knapper)
+val  BlaaSekundaer = Color (0xFF4D7AFF)
+val OnBlaa = Color (0xFFFFFFFF) // Hvit tekst på blå
+
+// Tertiær/aksenter
+val TurkisTertiaer = Color (0xFF4DEBFF)
+val OnTurkis = Color (0xFF000000) // Svart tekst på turkis
+
+// Bakgrunn
+val Bakgrunn = Color (0xFF666C80)
+val BakgrunnAlternativ = Color (0xFF667D80)
+val OnBakgrunn = Color(0xFFFFFFFF) // Hvit tekst på mørk bakgrunn
+
+// Nøytrale farge (dialogboks, rammer osv)
+val NoytralBrun = Color (0xFF807566)
+val OnNoytralBrun = Color(0xFFFFFFFF)
+
+
+
+
+
--- app/src/main/java/com/example/numbino_s305896/ui/theme/Theme.kt
+++ app/src/main/java/com/example/numbino_s305896/ui/theme/Theme.kt
@@ -12,45 +12,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = OransjeHovedfarge,
+    onPrimary = OnOransje,
+
+    secondary = BlaaSekundaer,
+    onSecondary = OnBlaa,
+
+    tertiary = TurkisTertiaer,
+    onTertiary = OnTurkis,
+
+    background = Bakgrunn,
+    onBackground = OnBakgrunn,
+
+    surfaceVariant = BakgrunnAlternativ,
+
+    surface = NoytralBrun,
+    onSurface = OnNoytralBrun
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
 
 @Composable
 fun Numbino_s305896Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = true,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
+    val colorScheme = DarkColorScheme
+    MaterialTheme (
         colorScheme = colorScheme,
         typography = Typography,
         content = content
